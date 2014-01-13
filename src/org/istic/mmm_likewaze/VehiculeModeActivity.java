8f5eb12922ed573317cc212f39cd19461f4b493f
Laurent Legendre <legendre.laurent50@gmail.com>
2014-01-13 12:16:45 +0100

    Adding map feature with default marker and current position FIX : changing API Key

--- src/org/istic/mmm_likewaze/VehiculeModeActivity.java
+++ src/org/istic/mmm_likewaze/VehiculeModeActivity.java
@@ -1,15 +1,66 @@
 package org.istic.mmm_likewaze;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.widget.Toast;
 
-public class VehiculeModeActivity extends Activity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class VehiculeModeActivity extends FragmentActivity {
+	// latitude and longitude
+	double latitude;
+	double longitude;
+	 
+	// Google Map
+	private GoogleMap googleMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_vehicule_mode);
+		try {
+			// Loading map
+			initilizeMap();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * function to load map. If map is not created it will create it for you
+	 * */
+	private void initilizeMap() {
+		if (googleMap == null) {
+			googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+					R.id.map)).getMap();
+
+			// Allow to show current position (blue dot)
+			googleMap.setMyLocationEnabled(true);
+			
+			// create marker
+			MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Hello Maps ");
+			 
+			// adding marker
+			googleMap.addMarker(marker);
+			
+			// check if map is created successfully or not
+			if (googleMap == null) {
+				Toast.makeText(getApplicationContext(),
+						"Sorry! unable to create maps", Toast.LENGTH_SHORT)
+						.show();
+			}
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		initilizeMap();
 	}
 
 	@Override
